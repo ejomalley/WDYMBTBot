68aca451b46986a211c1039d766cd9245c2d7da8
Evan O'Malley <55038905+ejomalley@users.noreply.github.com>
2022-03-03 00:01:27 +0000

    Added anti-spam measures

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WDYMBTBot
 
 A simple discord bot for my friends and I to use. Adds a command '?huh' that has the bot join your voice channel and play a funny audio clip (one of several options).
+
+The link to add to your server is: https://discord.com/api/oauth2/authorize?client_id=948055722057433129&permissions=3156992&scope=bot
--- main.py
+++ main.py
@@ -4,25 +4,29 @@ import os  # to get bot token from repl.it env vars
 import time  # for to sleep
 from keep_alive import keep_alive  # to keep bot server up
 keep_alive()
-
+running = False
 bot = commands.Bot(command_prefix='?')  # create bot with command prefix '?'
 
 @bot.command()  # command called 'huh'
 async def huh(ctx, v = 'druski'):
+  global running
+  if running: return
+  else: running = True
   if ctx.author.voice:  # if user who typed command is in a vc
-    
     voice = 'wdymbt-'+ v +'.wav' # set audio file (def druski)
 
     try:  # see if audio file exists
       track = Track(WAVAudio(voice), 'huh')  # if it does, create a Track from it
     except:  # if not, notify user 
       await ctx.send(f'nice try, but \'{v}\' is not an available voice')
+      running = False
       return
 
     # try to join voice channel
     try: mc = await ctx.author.voice.channel.connect(cls=MusicClient)
     except: # if cant join vc
       await ctx.send('couldnt join channel :(')  # send chat message
+      running = False
       return  # stop command
       
     await mc.play(track)  # if in vc, play wav audio file
@@ -31,6 +35,7 @@ async def huh(ctx, v = 'druski'):
     await mc.disconnect()  # leave channel after audio is played
     
     await ctx.message.delete()  # delete command from chat
+    running = False
   else: await ctx.send('youre not in a vc >:(')  # if user is not in vc, send chat
 
 bot.run(os.environ['TOKEN'])  # start bot
